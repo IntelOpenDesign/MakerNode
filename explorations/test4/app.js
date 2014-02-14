1c3164ed920328c7d590c4103d6941e5b8e0f1ce
Noura <culurienneldoreth@gmail.com>
2014-02-14 12:09:46 -0800

    trying out tap and target with jquery

---
+++ explorations/test4/app.js
@@ -0,0 +1,40 @@
+jsPlumb.bind('ready', function() {
+    console.log('hey there');
+    jsPlumb.Defaults.Container = $('#field');
+    var $field = $('#field');
+    var w = $field.width();
+    var h = $field.height();
+    var $els = [];
+    var points = [];
+    for (var i = 1; i <= 7; i++) {
+        $els[i] = $('#el'+i).css({
+            top: 1/7*(i-1)*h,
+            left: 1/7*(i-1)*w,
+            width: 1/7*.8*w,
+            height: 1/7*.8*h,
+        });
+    }
+
+    jsPlumb.makeSource($('.el'));
+    jsPlumb.makeTarget($('.el'), {anchor: 'Continuous'});
+
+    $('.el').on('mousedown', function(ev) {
+        console.log('mousedown');
+        var $this = $(this);
+        var $others = $('.el.highlight');
+        if ($others.length > 1) {
+            console.log('more than one highlighted el found');
+            alert('BUG: too many activated elements. deactivating all of them.');
+            $('.el').removeClass('highlight');
+        } else if ($others.length === 1 && $others.first().attr('id') !== $this.attr('id')) {
+            console.log('exactly one highlighted el found, and it is not the same as the one we just clicked');
+            var $other = $others.first();
+            jsPlumb.connect({source:$other, target:$this});
+            $('.el').removeClass('highlight');
+        } else {
+            console.log('no highlighted el found, or there is one but it is the one we just clicked');
+            $this.toggleClass('highlight');
+        }
+    });
+
+});
