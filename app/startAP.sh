a20f1f09bf673e4bbcd998dc32abbac486ed6698
Adam Pasztory <avatar@pasz.com>
2014-06-08 23:03:12 +0000

    Updated to new busybox path and hostapd path.

--- app/startAP.sh
+++ app/startAP.sh
@@ -1,6 +1,6 @@
 ifconfig wlan0 up
 wpa_cli -iwlan0 "terminate" &
-hostapd -B /etc/hostapd/hostapd.conf &
+hostapd -B /etc/hostapd.conf &
 ifconfig wlan0 192.168.0.10 &
-/home/root/busybox_custom dnsd -i 192.168.0.10 -c /etc/dnsd.conf &
-/home/root/busybox_custom udhcpd /etc/udhcpd.conf &
+busybox dnsd -i 192.168.0.10 -c /etc/dnsd.conf &
+busybox udhcpd /etc/udhcpd.conf &
