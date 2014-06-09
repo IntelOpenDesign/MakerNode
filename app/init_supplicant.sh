48c19b2455adb032154eee587be25a639216e334
Noura <culurienneldoreth@gmail.com>
2014-06-09 09:19:40 +0000

    trying to write a decent init_supplicant to get us connected to the wifi network

--- app/init_supplicant.sh
+++ app/init_supplicant.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo INIT_SUPPLICANT
+
 wpa_passphrase $1 << EOF > /etc/wpa_supplicant.conf
 $2
 EOF
@@ -7,8 +9,3 @@ EOF
 ifdown wlan0
 
 /etc/init.d/networking restart
-
-ifdown wlan0
-
-ifup wlan0
-
