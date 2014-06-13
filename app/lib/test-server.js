c091e1925ec716cd7e5a662836851ed233433852
Noura <culurienneldoreth@gmail.com>
2014-06-12 19:01:33 -0700

    made test-server use the pinModel as its message instead of a test message string

--- app/lib/test-server.js
+++ app/lib/test-server.js
@@ -2,9 +2,6 @@ var http = require('./http')();
 http.listen(8000);
 
 var socket = require('./socket').create();
-socket.create(function() {
-    var model = socket.getMessage();
-});
-socket.setMessage('test message for websocket from server');
+socket.create(function() {});
 
 
