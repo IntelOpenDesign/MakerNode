241a03f63482a15ec48ae6b07e6e2e5683349702
Adam Pasztory <avatar@pasz.com>
2014-07-04 09:22:54 -0700

    Add client todo generation

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -2,10 +2,9 @@ var gulp = require('gulp');
 var todo = require('gulp-todo');
 
 gulp.task('todo', function() {
-    gulp.src('lib/**/*.js')
-        .pipe(todo())
-        .pipe(gulp.dest('./'));
+  gulp.src(['lib/**/*.js', 'client/**/*.js'])
+    .pipe(todo())
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', ['todo']);
-
