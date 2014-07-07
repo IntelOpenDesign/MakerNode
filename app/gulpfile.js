59e563394e94749a2a0a31dfe5da50327ec30829
Noura <culurienneldoreth@gmail.com>
2014-07-07 13:31:31 -0700

    default gulp task now also reads the TODOs from the client side js and writes them to client/todo.md

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -5,6 +5,9 @@ gulp.task('todo', function() {
     gulp.src('lib/**/*.js')
         .pipe(todo())
         .pipe(gulp.dest('./'));
+    gulp.src('client/static/js/app.js')
+        .pipe(todo())
+        .pipe(gulp.dest('./client/'));
 });
 
 gulp.task('default', ['todo']);
