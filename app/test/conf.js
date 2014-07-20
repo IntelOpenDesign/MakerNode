348f3c3b1ebd6495d211eafa954df240e4bf5596
Adam Pasztory <avatar@pasz.com>
2014-07-20 11:30:46 -0700

    config file for coverage, and added new test file for conf.js

---
+++ app/test/conf.js
@@ -0,0 +1,12 @@
+var conf = require('../lib/conf')();
+var chai = require('chai'),
+  should = chai.should(),
+  expect = chai.expect;
+
+describe('Conf.read()', function() {
+  it('* Should work ', function(done) {
+    expect(true);
+    done();
+  });
+});
+
