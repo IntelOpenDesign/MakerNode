6889e6c8ccefd46644f23a436fa3ed99237876d7
Adam Pasztory <avatar@pasz.com>
2014-07-20 11:07:27 -0700

    code cleanup

--- app/test/http.js
+++ app/test/http.js
@@ -7,45 +7,39 @@ var chai = require('chai'),
 var PORT = 1234;
 var NETSTAT = "netstat -a -n | egrep '" + PORT + ".* LISTEN'";
 
-describe('HTTP', function() {
-  describe('#listen()', function() {
-    http.listen(PORT);
-    it('should listen on port ' + PORT, function(done) {
-      exec(NETSTAT, function(error, stdout, stderr) {
-        expect(stdout).to.not.have.length(0);
-        done();
-      });
+describe('HTTP.listen()', function() {
+  http.listen(PORT);
+  it('* Should listen on port ' + PORT, function(done) {
+    exec(NETSTAT, function(error, stdout, stderr) {
+      expect(stdout).to.not.have.length(0);
+      done();
     });
-    it('should load valid index.html', function(done) {
-      require('http').request({
-          host: '0.0.0.0',
-          port: PORT
-        },
-        function(response) {
-          var str = '';
-          response.on('data', function(chunk) {
-            str += chunk;
+  });
+  it('* Should load valid index.html', function(done) {
+    require('http').request({
+        host: '0.0.0.0',
+        port: PORT
+      },
+      function(response) {
+        var str = '';
+        response.on('data', function(chunk) {
+          str += chunk;
+        });
+        response.on('end', function() {
+          expect(str).to.have.string('<!DOCTYPE html>');
+          done();
+          describe('HTTP.close()', function() {
+            http.close(PORT);
+            it('* Should not listen on port ' + PORT, function(done) {
+              exec(NETSTAT, function(error, stdout, stderr) {
+                expect(stdout).to.have.length(0);
+                done();
+              });
+            });
           });
-          response.on('end', function() {
-            expect(str).to.have.string('<!DOCTYPE html>');
-            done();
-            test_close();
 
-          });
-        }
-      ).end();
-    });
+        });
+      }
+    ).end();
   });
 });
-
-function test_close() {
-  describe('#close()', function() {
-    http.close(PORT);
-    it('should not listen on port ' + PORT, function(done) {
-      exec(NETSTAT, function(error, stdout, stderr) {
-        expect(stdout).to.have.length(0);
-        done();
-      });
-    });
-  });
-}
