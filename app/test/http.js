e75d27f83d0d29ba6447a33d9114a35816c47ecf
Adam Pasztory <avatar@pasz.com>
2014-07-21 22:41:47 -0700

    Added test for socket.js

--- app/test/http.js
+++ app/test/http.js
@@ -1,23 +1,22 @@
 var http = require('../lib/http')();
 var exec = require('child_process').exec;
-var chai = require('chai'),
-  should = chai.should(),
-  expect = chai.expect;
+var should = require('chai').should();
 
+var HOST = '127.0.0.1';
 var PORT = 1234;
 var NETSTAT = "netstat -a -n | egrep '" + PORT + ".* LISTEN'";
 
 describe('HTTP.listen()', function() {
   http.listen(PORT);
-  it('* Should listen on port ' + PORT, function(done) {
+  it('* Listen on port ' + PORT, function(done) {
     exec(NETSTAT, function(error, stdout, stderr) {
-      expect(stdout).to.not.have.length(0);
+      stdout.should.not.have.length(0);
       done();
     });
   });
   it('* Should load valid index.html', function(done) {
     require('http').request({
-        host: '0.0.0.0',
+        host: HOST,
         port: PORT
       },
       function(response) {
@@ -26,13 +25,13 @@ describe('HTTP.listen()', function() {
           str += chunk;
         });
         response.on('end', function() {
-          expect(str).to.have.string('<!DOCTYPE html>');
+          str.should.have.string('<!DOCTYPE html>');
           done();
           describe('HTTP.close()', function() {
             http.close(PORT);
             it('* Should not listen on port ' + PORT, function(done) {
               exec(NETSTAT, function(error, stdout, stderr) {
-                expect(stdout).to.have.length(0);
+                stdout.should.have.length(0);
                 done();
               });
             });
