c9cf5a51e8fcf3ed0f3b837dfe21cfa93d50fc3f
Adam Pasztory <avatar@pasz.com>
2014-07-21 20:12:40 -0700

    conf.js test read/write working now

--- app/test/conf.js
+++ app/test/conf.js
@@ -3,43 +3,47 @@ var conf = require('../lib/conf').create();
 var chai = require('chai'),
   should = chai.should(),
   expect = chai.expect;
-var READ_FILE = 'default_appstate.conf';
+var READ_FILE = 'foo.temp';
 var WRITE_FILE = '';
+var rand = Math.random();
 
-describe('Conf()', function() {
-  describe('.read()', function() {
-    it('* Reads valid JSON from ' + READ_FILE, function(done) {
-      conf.read(READ_FILE)
-        .then(
-          function(state) {
-            state.should.have.property('app_mode', 'setup');
-            done();
-          },
-          function(reason) {
-            throw ('read error: ' + reason);
-          }
-      )
-    });
-  });
+//First, delete file.
+function deleteFile() {
+fs.unlink(READ_FILE, function(err) {
+  if (err) throw err;
+});
+}
 
-  describe('.write()', function() {
-    it('* Writes  ' + WRITE_FILE, function(done) {
-      fs.createReadStream(READ_FILE).pipe(fs.createWriteStream(READ_FILE + '.temp'));
-      conf.write(JSON.stringify({
-        test: 'pass'
-      }), 'foo.temp')
-        .then(
-          function(state) {
-           /* fs.unlink(READ_FILE + '.temp', function(err) {
-              if (err) throw err;
-            });*/
-            done();
-          },
-          function(reason) {
-            throw ('write error: ' + reason);
-          }
-      )
-    });
+describe('Conf.write()', function() {
+  deleteFile();
+  it('* Writes  ' + WRITE_FILE, function(done) {
+    // fs.createReadStream(READ_FILE).pipe(fs.createWriteStream(READ_FILE + '.temp'));
+    conf.write('foo.txt', JSON.stringify({
+      test: 'pass'
+    }))
+      .then(
+        function(state) {
+          done();
+
+          describe('Conf.read()', function() {
+            it('* Reads valid JSON from ' + READ_FILE, function(done) {
+              conf.read(READ_FILE)
+                .then(
+                  function(state) {
+                    state.should.have.property('test', 'pass');
+                    done();
+                    deleteFile();
+                  },
+                  function(reason) {
+                    throw ('read error: ' + reason);
+                  }
+              )
+            });
+          });
+        },
+        function(reason) {
+          throw ('write error: ' + reason);
+        }
+    )
   });
 });
-
