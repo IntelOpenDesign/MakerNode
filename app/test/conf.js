078b36bd2984d838f802a9f528927418d59a92bf
Adam Pasztory <avatar@pasz.com>
2014-07-21 17:13:29 -0700

    testing conf.read

--- app/test/conf.js
+++ app/test/conf.js
@@ -1,12 +1,21 @@
-var conf = require('../lib/conf')();
+var conf = require('../lib/conf').create();
 var chai = require('chai'),
   should = chai.should(),
   expect = chai.expect;
+var FILE = 'default_appstate.conf';
 
 describe('Conf.read()', function() {
-  it('* Should work ', function(done) {
-    expect(true);
-    done();
+  it('* Reads ' + FILE, function(done) {
+    conf.read(FILE)
+      .then(
+        function(state) {
+          state.should.have.property('app_mode', 'setup');
+          done();
+        },
+        function(reason) {
+          throw('read error: ' + reason);
+        }
+    )
   });
-});
 
+});
