4f99866063004547961e7e8ae9236719dd65386f
Adam Pasztory <avatar@pasz.com>
2014-07-27 00:52:56 -0700

    basic functionality for network_utils

--- app/lib/network_utils.js
+++ app/lib/network_utils.js
@@ -3,21 +3,31 @@ var sh = require('./command_queue').init().enqueue;
 var log = require('./log').create('network_utils');
 
 module.exports = function() {
-
   function start_access_point() {
-
+    sh('./startAP.sh'); //TODO: I'd like this to be asynchronous...
   }
-  
-  function stop_access_point() {
 
+  function stop_access_point() {
+    sh('killall hostapd');
   }
 
   function start_supplicant(ssid, password, gateway_ip, static_ip) {
-
+    var our_command = './init_supplicant.sh ' + ssid + ' ' + password;
+    if (gateway_ip && static_ip) {
+      our_command += ' ' + static_ip + ' ' + gateway_ip;
+    }
+    log.info('Attempting to init wlan0 with command: ' + our_command);
+    exec(our_command, function(error, stdout, stderr) {
+      if (error !== null) {
+        log.error('Error starting supplicant.', 'error=' + error, 'stdout=' + stdout, 'stderr=' + stderr);
+      }
+    });
   }
 
   function stop_supplicant() {
-
+    //TODO
+    //It seems like this is just
+    sh('./restore_factory_settings.sh');
   }
 
   return {
@@ -27,3 +37,5 @@ module.exports = function() {
     stop_supplicant: stop_supplicant
   };
 }
+
+//TODO: It would be nice to support callbacks on these.
