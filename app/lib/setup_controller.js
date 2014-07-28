331d43d771045f3c31700b26da0a401fb1287cba
Noura <culurienneldoreth@gmail.com>
2014-07-28 11:55:26 -0700

    changing the log interface in setup_controller to be correct, adding a little bit more logging

--- app/lib/setup_controller.js
+++ app/lib/setup_controller.js
@@ -2,33 +2,42 @@
 var io = require('socket.io');
 var sh = require('./command_queue').init().enqueue;
 var _ = require('underscore');
-var log = require('./log').create('setup_controller');
+var log = require('./log')('setup_controller');
 
 module.exports = function(state, wss) {
   var onUpdate;
   var on_finished;
 
   function start(port) {
-    log.info('WebSockets Serving listening on port ' + port);
+    log.info('Start');
 
     wss.on('connect', function(conn) {
+      log.debug('client connected');
 
       conn.on('confirm_mac', function(d) { // STEP 1
+        log.debug('got confirm_mac info', JSON.stringify(d));
         state.network_confirmed = true;
       });
 
       conn.on('create_user', function(d) { // STEP 2
+        log.debug('got create_user info', JSON.stringify(d));
         state.user_password_set = true;
       });
 
-      conn.on('router_setup' function(d) { // STEP 3
-        if (_.has(d, 'wifi_ssid') && _.has(d, 'wifi_password')) {
+      conn.on('router_setup', function(d) { // STEP 3
+        log.debug('got router info', JSON.stringify(d));
+        if (_.has(d, 'ssid') && _.has(d, 'pwd')) {
           state.router_ssid = d.wifi_ssid;
           state.router_password = d.wifi_password;
           on_finished(state);
         }
       });
+
+      conn.on('disconnect', function(d) {
+          log.debug('client disconnected');
+      });
     });
+
   }
 
   function stop() {
