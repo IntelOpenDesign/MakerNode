29c74ab312b7170bccf39df9e762262387fb50fc
Noura <culurienneldoreth@gmail.com>
2014-07-30 13:01:53 -0700

    lib/log.js always uses the file prefix when logging, and it takes any number of arguments, and it logs JSON objects

--- app/lib/log.js
+++ app/lib/log.js
@@ -20,16 +20,43 @@ var log_types = {
 };
 
 function log(name) {
+    var have_written_prefix = false;
+
+    var write_msg = function(t, msg) {
+        if (msg !== '') {
+            var prefix = '[' + name + ':' + t.prefix + ']';
+            console[t.console](clc[t.color].bold(prefix) + clc[t.color](msg));
+            have_written_prefix = true;
+        }
+    };
+
+    var write_obj = function(t, obj) {
+        if (!have_written_prefix) {
+            write_msg(t, ' ');
+        }
+        console[t.console](obj);
+    };
+
     var logger = function(type) {
         // TODO make sure this prints JSON nicely
         var t = log_types[type];
         return function() {
-            var prefix = '[' + name + ':' + t.prefix + ']';
+            have_written_prefix = false;
             var msg = '';
             for (var i = 0; i < arguments.length; i++) {
-                msg += ' ' + arguments[i];
+                var data_type = typeof(arguments[i]);
+                if (data_type === "string" ||
+                    data_type === "number" ||
+                    data_type === "undefined" ||
+                    arguments[i] === null) {
+                    msg += ' ' + arguments[i];
+                } else {
+                    write_msg(t, msg);
+                    write_obj(t, arguments[i]);
+                    msg = '';
+                }
             }
-            console[t.console](clc[t.color].bold(prefix) + clc[t.color](msg));
+            write_msg(t, msg);
         };
     };
 
