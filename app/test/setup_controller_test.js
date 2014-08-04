47ec0b6bbd89ebe77fa29325faffa8367c0cbfbd
Adam Pasztory <avatar@pasz.com>
2014-08-04 15:54:11 -0700

    Trying to get test working again...

--- app/test/setup_controller_test.js
+++ app/test/setup_controller_test.js
@@ -3,33 +3,32 @@ var io = require('socket.io-client');
 var exec = require('child_process').exec;
 var should = require('chai').should();
 
-var setup_state = {};
-var setup_controller = require('../lib/setup_controller')(setup_state);
+var network_utils = require('../lib/network_utils')();
+var state = {
+  "setup_state": {
+    "network_confirmed": false,
+    "user_password_set": false,
+    "ssid": "",
+    "pwd": "",
+    "router_gateway_ip": "",
+    "galileo_static_ip": ""
+  }
+};
+
 var client;
 var HOST = 'localhost';
 var PORT = 8001;
 var NETSTAT = "netstat -a -n | egrep '" + PORT + ".* LISTEN'"; //TODO: Make this a util method
 
-/*socketOptions = {
-  transports: ['websocket'],
-  'force new connection': true
-  }*/
-describe('setup_controller.start()', function() {
-  setup_controller.set_on_finished(function(state) {
-    describe('setup_controller.on_finished()', function() {
-      it('* Messages should update state correctly', function(done) {
-        state.network_confirmed.should.equal(true);
-        done();
-      client.close();
-      });
-    });
-  });
+var servers = network_utils.create_servers(PORT);
+var setup_controller = require('../lib/setup_controller')(state, servers.socketio_server, on_finished, on_redirect);
 
-  setup_controller.start(PORT);
+describe('setup_controller.start()', function() {
+
+  setup_controller.start();
   it('* Should listen on port ' + PORT, function(done) {
     exec(NETSTAT, function(error, stdout, stderr) {
       stdout.should.not.have.length(0);
-
       done();
     });
   });
@@ -38,23 +37,16 @@ describe('setup_controller.start()', function() {
     var message_count = 0;
     client = io('http://' + HOST + ':' + PORT);
     client.on('connect', function() {
-      client.emit('event', '{"mac_address":"12345"}');
-      message_count++;
+      client.emit('confirm_mac', {mac_address:"12345"});
+      setTimeout(function() {
+        client.emit('create_user', {user_password:"boo", username:"who"});
+      }, 400);
+      setTimeout(function() {
+        client.emit('router_setup', {ssid:"cat", pwd:"meow"});
+      }, 400);
+      g_done = done; //HACK
     });
-
-
-    client.on('event', function(data, flags) {
-      if (message_count == 1) {
-        client.emit('event', '{"user_password":"boo", "username":"who"}');
-      }
-      if (message_count == 2) {
-        client.emit('event', '{"wifi_ssid":"cat", "wifi_password":"meow"}');
-        done();
-      }
-      message_count++;
-      console.log('DATA: ' + data);
-    });
-
+    
     client.on('disconnect', function() {
       describe('setup_controller.stop()', function() {
         it('* Socket should close', function(done) {
@@ -69,3 +61,17 @@ describe('setup_controller.start()', function() {
     });
   });
 });
+
+var g_done;
+function on_finished(state) {
+  console.log("on_finished");
+  g_done();
+  describe('setup_controller.on_finished()', function() {
+    it('* Messages should update state correctly', function(done) {
+      state.network_confirmed.should.equal(true);
+      client.close();
+    });
+  });
+}
+
+function on_redirect() {}
