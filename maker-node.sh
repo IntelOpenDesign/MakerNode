f6ae6e0bf0cdb2493010e98975798357d8b56213
Noura <culurienneldoreth@gmail.com>
2014-08-11 15:24:43 -0700

    moved everything out of the app subdirectory

--- maker-node.sh
+++ maker-node.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 start() {
         echo "Starting MakerNode Server..."
-        pushd /home/root/mn_setup/app 
+        pushd /home/root/mn_setup
         node ./index.js
         popd
 }
