2d4e87fe4625c26e30db1027c910f78a6cc20631
Noura <culurienneldoreth@gmail.com>
2014-08-18 08:12:50 -0700

    changed the dnsd command slightly

--- startAP.sh
+++ startAP.sh
@@ -2,5 +2,5 @@ ifconfig wlan0 up
 wpa_cli -iwlan0 "terminate" &
 hostapd -B /etc/hostapd.conf &
 ifconfig wlan0 192.168.0.10 &
-busybox dnsd -i 192.168.0.10 -c /etc/dnsd.conf &
+busybox dnsd -i 192.168.0.10 -p 53 -c /etc/dnsd.conf &
 busybox udhcpd /etc/udhcpd.conf &
