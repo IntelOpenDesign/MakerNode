29ac25975762b1e5e3de2e8187dde507b156c362
Seth Hunter <hunters@mit.edu>
2014-09-22 17:34:48 -0700

    adding shell script to install iotdk libs

---
+++ install_iot_libs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "installing hostapd, libnl 1.1, and busybox-i586 binaries"
+cd iotkd_libs/
+cp libnl.so.1 /usr/lib
+cp hostapd /usr/bin
+cp hostapd_cli /usr/bin
+cp busybox-i586 /usr/bin
