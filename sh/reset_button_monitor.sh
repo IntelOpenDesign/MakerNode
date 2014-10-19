6de641b0baca74c637ec32ce49a634defaf86ad9
Seth Hunter <hunters@mit.edu>
2014-10-19 18:05:53 +0200

    fixed the shell script and tested with the iotdk, still need to add it to systemd

--- sh/reset_button_monitor.sh
+++ sh/reset_button_monitor.sh
@@ -11,9 +11,24 @@ inWaitMode=false
 gpioPressed="0"
 input_reset_gpio=
 output_reset_gpio=
+board=
 
-type dmidecode > /dev/null 2>&1 || die "dmidecode not installed"
-board=$(dmidecode -s baseboard-product-name)
+#get the distribution of Linux
+issue=$(cat /etc/issue | awk '{print $1;}' | tr -d "\n")
+echo "linux distribution is: $issue"
+
+if [ "$issue" == "iot-devkit" ]; then
+	#method for iotdk dist. by SSG (full gcc)
+	board=$(cat /sys/devices/virtual/dmi/id/board_name)
+	echo "board name is: $board"
+else
+	#method for standard version of linux dist. by NDG 
+	type dmidecode > /dev/null 2>&1 || die "dmidecode not installed"
+	board=$(dmidecode -s baseboard-product-name)
+	echo "board name is: $board"
+fi
+
+#get the GPIO pins for the button
 case "$board" in
     "Galileo")
         input_reset_gpio=52
@@ -33,8 +48,12 @@ startAPMode()
   echo "Board returned to original setting and is now in AP mode"
 }
 
-#output the pid of this process to file
-echo $$ > reset.pid
+#output the pid of this process to file (no longer needed)
+#echo $$ > reset.pid
+
+#make sure the GPIO is exported
+echo -n $output_reset_gpio > /sys/class/gpio/export
+echo -n $input_reset_gpio > /sys/class/gpio/export
 
 #check button press of buttons
 keepgoing=true
@@ -42,7 +61,8 @@ while $keepgoing
 
 do
 	if [ "$inWaitMode" == false ]; then
-	
+		
+		
 		gpioIn=`cat /sys/class/gpio/gpio${output_reset_gpio}/value`
 		gpioOut=`cat /sys/class/gpio/gpio${input_reset_gpio}/value`
 	
