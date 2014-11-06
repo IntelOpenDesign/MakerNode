d0eb1ae16278c0b2060625ffd5f879e60077c87b
Adam Pasztory <avatar@pasz.com>
2014-11-06 15:34:16 -0800

    Added rudimentary option parser and support for passing in port as an argument to index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-var server = require('./lib/app')();
-server.start();
+doc = function() {
+/*!
+Usage: 
+index.js [--port=<port>] 
+*/
+}
+var docopt = require ('docopt-js-shim');
+var args = docopt.fromComment(doc);
+console.log(args);
+var port = args['--port'];
+
+var app = require('./lib/app')();
+app.start(port ? port : 80);
