d5ff549e14941736d5f2e586f3466eb80b9645dd
Adam Pasztory <avatar@pasz.com>
2014-11-06 17:32:26 -0800

    Added maker_cat app.

---
+++ apps/maker_cat/lib/gpio.js
@@ -0,0 +1,92 @@
+var log = require('../../../lib/utils/log')('GPIO');
+var Galileo = require("galileo-io");
+var Q = require('q');
+var _ = require('underscore');
+var board;
+var OUTPUT_SCALE = 255;
+
+function GPIO() {}
+
+GPIO.prototype.init = init;
+GPIO.prototype.refreshOutputs = refreshOutputs;
+
+module.exports = function(){
+  return new GPIO();
+}
+
+function init(onInput) {
+    log.debug('init gpio');
+    var deferred = Q.defer();
+    board = new Galileo();
+    board.on("ready", function() {
+        log.info("BOARD READY");
+        var byte = 0;
+        this.pinMode(9, this.MODES.OUTPUT);
+        board.analogRead('A0', function(data) {
+            onInput(14, data);
+        });
+        board.analogRead('A1', function(data) {
+            onInput(15, data);
+        });
+        board.analogRead('A2', function(data) {
+            onInput(16, data);
+        });
+        board.analogRead('A3', function(data) {
+            onInput(17, data);
+        });
+        board.analogRead('A4', function(data) {
+            onInput(18, data);
+        });
+        board.analogRead('A5', function(data) {
+            onInput(19, data);
+        });
+        //TODO: Figure out why bind wasn't working here. :/
+        deferred.resolve(board);
+    });
+    board.on('error', function(reason) {
+        deferred.reject(reason);
+    });
+    return deferred.promise;
+}
+
+var cachedPinValues = [];
+
+function refreshOutputs(model) {
+    _.each(model.connections,
+        function(connection) {
+            var source = connection.source;
+            var target = connection.target;
+            //console.log(source + "->" + target + " value=" + model.pins[target].value );
+            model.pins[target].value = model.pins[source].value;
+        }
+        //TODO: AND values for multiple inputs
+    );
+
+    var i = 0;
+    _.each(model.pins,
+        function(pin) {
+            var scaledValue;
+            if (pin.is_analog) {
+                scaledValue = Math.round(pin.value * OUTPUT_SCALE)
+            } else {
+              //TODO: No, need to read min/max from input pin
+                var threshold = pin.input_min + (pin.input_max - pin.input_min) / 2;
+                scaledValue = pin.value < threshold ? 0 : OUTPUT_SCALE;
+            }
+            if (pin.is_inverted) {
+              scaledValue = 255 - scaledValue;
+            }
+            if (!pin.is_input && cachedPinValues[i] != scaledValue) {
+                log.debug("Changed pin=" + i + " oldValue=" + cachedPinValues[i] + " newValue=" + scaledValue);
+                write(pin, i, scaledValue);
+            }
+            cachedPinValues[i] = scaledValue;
+            i++;
+        }
+    );
+}
+
+function write(pin, pinIndex, scaledValue) {
+    pin.is_analog ? board.analogWrite(pinIndex, scaledValue) : board.digitalWrite(pinIndex, scaledValue);
+
+}
---
+++ apps/maker_cat/lib/socket.js
@@ -0,0 +1,195 @@
+var _ = require('underscore');
+var log = require('../../../lib/utils/log')('Socket');
+var WebSocketServer = require('ws').Server;
+var exec = require('child_process').exec;
+
+var onUpdate;
+var msg;
+
+function Socket(){
+    //TODO: Move initialization here...
+    msg = {
+        status: 'OK', // TODO test client side with "error" status
+        pins: {},
+        connections: [],
+        count: 0, // TODO remove when done debugging
+        message_ids_processed: [],
+        ssid: 'ConnectAnything',
+    };
+    _.each(digital_outs, pin_setter(false, false));
+    _.each(digital_ins, pin_setter(false, true));
+    _.each(analog_outs, pin_setter(true, false));
+    _.each(analog_ins, pin_setter(true, true));
+}
+
+Socket.prototype.create = create;
+Socket.prototype.getMessage = getMessage;
+Socket.prototype.setMessage = setMessage;
+
+module.exports = Socket;
+module.exports.create = function(){
+    return new Socket();
+}
+
+// pin defaults
+var digital_outs = ['1', '2', '3', '4', '7', '8', '12', '13'];
+var digital_ins = ['0'];
+var analog_outs = ['3', '5', '6', '9', '10', '11'];
+var analog_ins = ['14', '15', '16', '17', '18', '19']; // A0 - A5
+
+var all_pins = [].concat(digital_outs).concat(digital_ins).concat(analog_outs).concat(analog_ins);
+
+function setMessage(state) {
+    msg = state;
+}
+
+function getMessage() {
+    return msg;
+}
+
+var messages_dict = {};
+
+var N_CLIENTS = 0;
+
+var change_ssid_message_id = null;
+
+function pin_setter(is_analog, is_input) {
+    return function(id) {
+        msg.pins[id] = {
+            label: '',
+            value: 0,
+            is_visible: Math.random() > 0.5 ? true : false,
+            //is_visible: true,
+            is_analog: is_analog,
+            is_input: is_input,
+            input_min: 0.0,
+            input_max: 1.0,
+            damping: 0,
+            is_inverted: false,
+            is_timer_on: false,
+            timer_value: 0,
+        };
+    }
+}
+
+var broadcast_interval_id = 0;
+
+var onConnect = function(conn) {
+
+    N_CLIENTS += 1;
+    log.info('new connection, N_CLIENTS', N_CLIENTS);
+    if (!broadcast_interval_id) {
+        broadcast_interval_id = setInterval(function() {
+            msg.count += 1;
+            msg.message_ids_processed = _.keys(messages_dict);
+            try {
+                conn.send(JSON.stringify(msg));
+            } catch (error) {
+                log.info(error);
+                //TODO: do we need to recover here?
+            }
+            //log.info('sent message: ' + JSON.stringify(msg));
+            _.each(_.keys(messages_dict), function(id) {
+                messages_dict[id] += 1;
+            });
+            var message_ids_to_delete = _.filter(_.keys(messages_dict),
+                function(id) {
+                    return messages_dict[id] >= N_CLIENTS * 2;
+                });
+            _.each(message_ids_to_delete, function(id) {
+                delete messages_dict[id];
+                if (id === change_ssid_message_id) {
+                    clearInterval(broadcast_interval_id);
+                    log.info('clearing broadcast interval');
+                }
+            });
+        }, 33); //TODO: Figure out the optimal value here
+    }
+
+    conn.on('message', function(str) {
+        log.info('received ' + str);
+        setTimeout(function() {
+            //log.info('processing ' + str);
+            var d = JSON.parse(str);
+            _.each(d.connections, function(dc) {
+                var index = -1;
+                _.each(msg.connections, function(mc, i) {
+                    if (mc.source === dc.source && mc.target === dc.target) {
+                        index = i;
+                    }
+                });
+                // if client wants to add connection && we don't already have it
+                if (dc.connect && index < 0) {
+                    msg.connections.push({
+                        source: dc.source,
+                        target: dc.target
+                    });
+                    // if client wants to remove connection && we have it
+                } else if (!dc.connect && index >= 0) {
+                    msg.connections.splice(index, 1);
+                }
+            });
+            _.each(d.pins, function(pin, id) {
+                msg.pins[id].value = pin.value;
+                msg.pins[id].label = pin.label;
+                msg.pins[id].is_visible = pin.is_visible;
+                msg.pins[id].is_analog = pin.is_analog;
+                msg.pins[id].is_input = pin.is_input;
+                msg.pins[id].is_inverted = pin.is_inverted;
+                msg.pins[id].input_min = pin.input_min;
+                msg.pins[id].input_max = pin.input_max;
+                msg.pins[id].damping = pin.damping;
+                msg.pins[id].is_timer_on = pin.is_timer_on;
+                msg.pins[id].timer_value = pin.timer_value;
+            });
+            log.info('processing message ID', d.message_id);
+            messages_dict[d.message_id] = 0;
+            if (_.has(d, 'ssid') && d.ssid !== msg.ssid) {
+                // a user has changed the SSID. we should notify all other
+                // users and then stop broadcasting, to mimic the users having
+                // to reconnect to another wifi hotspot
+                msg.ssid = d.ssid;
+                var change_ssid_message_id = d.message_id;
+            }
+            onUpdate();
+        }, 0);
+    });
+
+    conn.on('close', function(code, reason) {
+        clearInterval(broadcast_interval_id);
+        broadcast_interval_id = 0;
+        try {
+            log.info('close - try');
+        } catch (e) {
+            log.info('close - catch');
+        } finally {
+            log.info('close - finally');
+        }
+    });
+    conn.on('end', function() {
+        try {
+            log.info('end - try');
+        } catch (e) {
+            log.info('end - catch');
+        } finally {
+            log.info('end - finally');
+        }
+    });
+    conn.on('error', function() {
+        log.info('error');
+    });
+}
+
+function create(callback) {
+    onUpdate = callback;
+
+    var port = 8001;
+    log.info('WebSockets Serving listening on port ' + port);
+    wss = new WebSocketServer({
+        port: port,
+    });
+    wss.on('connection', onConnect);
+    //for testing random data
+    //setInterval(update, 6000);
+}
+
