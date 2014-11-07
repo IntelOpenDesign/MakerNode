7ff76aa564a7099641fea3ad4f2dece95bbcc5f7
Adam Pasztory <avatar@pasz.com>
2014-11-06 16:08:34 -0800

    Integrate Next Steps and Dashboard.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ index.js [--port=<port>]
 }
 var docopt = require ('docopt-js-shim');
 var args = docopt.fromComment(doc);
-console.log(args);
 var port = args['--port'];
 
 var app = require('./lib/app')();
