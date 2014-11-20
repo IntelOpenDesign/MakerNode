e66185e875cf39f06c87ea5d2edc27f052a2b1de
Adam Pasztory <avatar@pasz.com>
2014-11-20 11:48:03 -0800

    Made hello world code easier to read.

--- apps/hello_world/app.js
+++ apps/hello_world/app.js
@@ -1,21 +1,27 @@
 var utils = require('../../lib/utils/galileo')();
-var log = require('../../lib/utils/log')('app')
 var path = require('path');
 var Galileo = require("galileo-io");
 var board = new Galileo();
 var pin = 13;
-var byte = 0;
-
 var HOST = 'localhost';
 var PORT = 3000;
 
 var servers = utils.create_servers(PORT, path.join(__dirname, '/client'));
-log.info('HTTP and WS servers listening on port', PORT);
+console.log('HTTP and WS servers listening on port', PORT);
 
-servers.socketio_server.on('connect', function(conn) {
-  log.debug('client connected');
-  conn.on('toggle_13', function(conn) {
-    log.debug('toggle_13');
-    board.digitalWrite(pin, (byte ^= 1));
-  });
-});
+servers.socketio_server.on('connect', handleConnect);
+
+function handleConnect(conn) {
+  console.log('client connected');
+  conn.on('pin_on', handlePinOn);
+  conn.on('pin_off', handlePinOff);
+}
+
+function handlePinOn() {
+  console.log('toggle_13 on');
+  board.digitalWrite(pin, 1);
+}
+function handlePinOff() {
+  console.log('toggle_13 off');
+  board.digitalWrite(pin, 0);
+}
